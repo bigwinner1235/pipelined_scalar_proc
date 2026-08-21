139859554f3afbbfbccae5173a8b1d0b84a27db3
Jack <JackRobbennolt@gmail.com>
2026-08-20 19:15:17 -0500

    closes 20ns :(

---
+++ rtl/ProcWrapper.v
@@ -0,0 +1,21 @@
+//improve synthesis
+module ProcWrapper (
+    input clk, rst,
+    output halt, illegal, write
+);
+
+reg sync_rst0, sync_rst;
+always @(posedge clk) begin
+    sync_rst0 <= rst;
+    sync_rst <= sync_rst0;
+end
+
+PipelinedTopLevel proc (
+    .reset(sync_rst),
+    .startpc(64'h0),
+    .clk(clk),
+    .halt(halt),
+    .illegal(illegal),
+    .data_mem_write_enable(write)
+);
+endmodule
